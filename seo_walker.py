dcbc1f90e968a3af77d0089ce1a26d0826aeca0b
Denizz <deniz1983@mail.ru>
2018-06-17 22:27:55 +0300

    walking functions added

--- seo_walker.py
+++ seo_walker.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
+
 class SeoWalker():
     def walk(self, url, amount):
         base_url = url
@@ -16,15 +17,20 @@ class SeoWalker():
 
         # с помощью цикла сделать поиск линков amount раз и сделать переход на них. Потом передать линк в write_to_file(link)
         a_links = driver.find_elements_by_xpath("//a[contains(@href,'"+url+"')] ")
-
+        links_to_write = []
         for i in range (0, len(a_links)):
-            print("link " + str(a_links[i].get_attribute("href")))
+            if len(str(a_links[i].get_attribute("href"))) > len(base_url) + 3:
+                links_to_write.append(str(a_links[i].get_attribute("href")))
+                #print("link " + str(a_links[i].get_attribute("href")))
 
-        print("The amount of links is " + str(len(a_links)))
+            if len(links_to_write) == 5:
+                self.walk_inside(links_to_write)
+                break
+        print("The amount of links is " + str(len(links_to_write)))
         driver.close()
-        pass
 
-    def walk_inside(self):
+    def walk_inside(self,links_array):
+        print(links_array)
         pass
 
     def write_to_file(self):
