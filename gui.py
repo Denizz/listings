42727efa4d6efafc44a1154e0919b323911d2154
Denizz <deniz1983@mail.ru>
2018-07-11 23:44:36 +0300

    new options added

--- gui.py
+++ gui.py
@@ -13,60 +13,72 @@ def main():
     win.title('SEO Walker V.1.0.0')
     win.resizable(False, False)
 
+    #tabs-menu
+    tabControl = ttk.Notebook(win)
+
+    simple_walk = ttk.Frame(tabControl)
+    tabControl.add(simple_walk, text=u'Простое посещение')
+    search_walk = ttk.Frame(tabControl)
+    tabControl.add(search_walk, text=u'Через поисковик')
+    tabControl.pack(expand=1, fill="both")
+
     #поле ввода адреса сайта
-    webaddress = Entry(win, width=30)
+    webaddress = Entry(simple_walk, width=30, font="Courier 20")
     webaddress.focus()
-    webaddress.grid(row=1, column=0)
-    lbl_address = Label(win, text="Название сайта начиная с http:// ")
-    lbl_address.grid(row=1, column=1)
+    webaddress.pack()
+    lbl_address = Label(simple_walk, text="Название сайта начиная с http:// ",
+    font="Courier 20")
+    lbl_address.pack()
 
     #поле ввода количества ссылок
-    ent_amount = Entry(win, width=2)
-    ent_amount.grid(row=2, column=0)
-    lbl_ent = Label(win, text="Количество ссылок")
-    lbl_ent.grid(row=2, column=1)
-
-    #чек-бокс производить ли поиск через поисковик
-    is_search = IntVar()
-    check1 = Checkbutton(win, text=u'Через поисковик', variable=is_search,
-    onvalue=1, offvalue=0)
-    check1.grid(row=3, column=0)
+    ent_amount = Entry(simple_walk, width=2, font="Courier 20")
+    ent_amount.pack()
+    lbl_ent = Label(simple_walk, text="Количество ссылок", font="Courier 20")
+    lbl_ent.pack()
 
     #select список
     searcher = StringVar()
-    searcher_chosen = ttk.Combobox(win, textvariable=searcher)
+    searcher_chosen = ttk.Combobox(search_walk, textvariable=searcher,
+    font="Courier 20")
     searcher_chosen['values'] = ("Yandex", "Google")
     searcher_chosen.current(0)
-    searcher_chosen.grid(row=3, column=1)
+    searcher_chosen.pack()
 
     #фраза для поиска
-    phrase = Entry(win, width=30)
-    phrase.grid(row=4, column=0)
-    lbl_phrase = Label(win, text="Фраза для поиска")
-    lbl_phrase.grid(row=4, column=1)
+    phrase = Entry(search_walk, width=30, font="Courier 20")
+    phrase.pack()
+    lbl_phrase = Label(search_walk, text="Фраза для поиска", font="Courier 20")
+    lbl_phrase.pack()
 
     #шаблон адреса для поиска
-    tmp_address = Entry(win, width=30)
-    tmp_address.grid(row=5, column=0)
-    lbl_tmp_address = Label(win, text="Шаблон названия сайта")
-    lbl_tmp_address.grid(row=5, column=1)
+    tmp_address = Entry(search_walk, width=30, font="Courier 20")
+    tmp_address.pack()
+    lbl_tmp_address = Label(search_walk, text="Шаблон названия сайта",
+    font="Courier 20")
+    lbl_tmp_address.pack()
 
-    #клики по рекламе
-    is_ads = IntVar()
-    check1 = Checkbutton(win, text=u'Кликнуть по рекламе', variable=is_ads,
-    onvalue=1, offvalue=0)
-    check1.grid(row=6, column=0)
+    #клики по рекламе1
+    is_ads1 = IntVar()
+    check1 = Checkbutton(search_walk, text=u'Кликнуть по рекламе',
+    variable=is_ads1, onvalue=1, offvalue=0, font="Courier 20")
+    check1.pack()
+
+    #клики по рекламе2
+    is_ads2 = IntVar()
+    check2 = Checkbutton(simple_walk, text=u'Кликнуть по рекламе',
+    variable=is_ads2, onvalue=1, offvalue=0, font="Courier 20")
+    check2.pack()
 
     #количество переходов по рекламе
-    ads_amount = Entry(win, width=2)
-    ads_amount.grid(row=6, column=1)
-    lbl_ads = Label(win, text="Количество переходов по рекламе")
-    lbl_ads.grid(row=6, column=2)
+    ads_amount = Entry(search_walk, width=2)
+    ads_amount.pack()
+    lbl_ads = Label(search_walk, text="Количество переходов по рекламе")
+    lbl_ads.pack()
 
     #кнопка выполнить
-    start_button = Button(win, text='Выполнить', width=10, height=2,
+    start_button = Button(simple_walk, text='Выполнить', width=10, height=2,
     command=start_walk)
-    start_button.grid(row=7, column=2)
+    start_button.pack()
 
     win.mainloop()
 
